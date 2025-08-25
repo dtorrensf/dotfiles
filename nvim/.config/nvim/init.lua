3b6e4684f8f6b1462ed1b25ce651afe93d58f8fe
Diego Torréns Farias <dtorrensfarias@outlook.com>
2025-08-25 21:09:20 +0200

    feat: add lazygit config for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -268,6 +268,27 @@ require("lazy").setup({
                 })
             end,
         },
+        -- Lazygit integration
+        {
+            "kdheepak/lazygit.nvim",
+            cmd = {
+                "LazyGit",
+                "LazyGitConfig",
+                "LazyGitCurrentFile",
+                "LazyGitFilter",
+                "LazyGitFilterCurrentFile",
+            },
+            -- optional for floating window border decoration
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+            },
+            -- setting the keybinding for LazyGit with 'keys' is recommended in
+            -- order to load the plugin when the command is run for the first time
+            keys = {
+                { "<leader>gg", "<cmd>LazyGit<cr>",            desc = "Abrir LazyGit" },
+                { "<leader>gc", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit archivo actual" },
+            }
+        },
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
